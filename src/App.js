8c929177be3958b84b32fb8b0e2ec8aaf905432d
Ben4 <benymany511@gmail.com>
2026-03-15 20:54:17 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Listing from './components/Listing';
-import etsyData from './data/etsy.json'; // Импорт данных
+import etsyData from './data/etsy.json';
 
 function App() {
   return (
